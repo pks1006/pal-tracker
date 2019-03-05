1a0b2f1defd594fc82bfe833c3b423c2225fe0c7
Piyush Shrivastava <piyush.cpp@gmail.com>
2019-03-05 12:08:57 -0500

    Setting Env Variable and trying again

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,27 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    Map<String, String> map = new HashMap<String, String>();
+
+    public EnvController(@Value("${port}") String port, @Value("${memory.limit}") String mem, @Value("${cf.instance.index}") String ind, @Value("${cf.instance.addr}") String add){
+        map.put("PORT",port);
+        map.put("MEMORY_LIMIT",mem);
+        map.put("CF_INSTANCE_INDEX",ind);
+        map.put("CF_INSTANCE_ADDR",add);
+    }
+
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv(){
+            return map;
+    }
+}
